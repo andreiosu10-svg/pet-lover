7428a83d18d56414c99a0d0f518f91a4f9001446
andreiosu10-svg <andreiosu10@gmail.com>
2026-02-01 07:31:16 +0200

    Update create-payment-intent.js

--- pages/api/create-payment-intent.js
+++ pages/api/create-payment-intent.js
@@ -1,11 +1,10 @@
 import Stripe from 'stripe';
 
-const stripe = new Stripe('sk_test_51SvrQSGI6cib5uhzWECkzQf3O607YLcmEoILeE7LVI2FNZbsYh4DYPzuYv0b6dmr4TMfUPjSLf9mjGQIWyJdAXhX00ZKzSJPpZ');
+const stripe = new Stripe('sk_test_51SvrQSGI6cib5uhzUB1zUUnP2zXZQ1wr0GMZHCUCjsfuByuXrVEz4wGvt25iZF0wAUJXUdV6CNUVffnNfFpJnDWB00Hbs9v8aW');
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { amount } = req.body;
-
     try {
       const paymentIntent = await stripe.paymentIntents.create({
         amount: amount,
